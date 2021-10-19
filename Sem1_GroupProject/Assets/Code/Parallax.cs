2cfc5715069f9beca8fe3f6cafec9463e3b6a3af
A00287047 <A00287047@student.ait.ie>
2021-10-19 23:43:25 +0100

    Parallax
    Adding code + Background for parallax effect

---
+++ Sem1_GroupProject/Assets/Code/Parallax.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float speed = 4f;
+    private Vector3 startpos;
+    void Start()
+    {
+        startpos = transform.position;
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        if (transform.position.x < -18.2f)
+        {
+            transform.position = startpos;
+        }
+
+    }
+}
